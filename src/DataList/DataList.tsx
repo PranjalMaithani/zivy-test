8e43231ed3efab589c091c15ffd0b4c4736e6eae
Pranjal Maithani <maithanipranjal@gmail.com>
2024-06-14 20:40:29 +0530

    infinite scrolling

--- src/DataList/DataList.tsx
+++ src/DataList/DataList.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useDataList from "./useDataList";
+import { useIntersectionObserver } from "usehooks-ts";
 
-const Footer = () => {
-  return <p>LOADING...</p>
+const Footer = ({onScreenCallback, loading = false}) => {
+  const {isIntersecting, ref} = useIntersectionObserver();
+
+  useEffect(() => {
+    if(isIntersecting && !loading) {
+      onScreenCallback();
+    }
+  }, [isIntersecting, loading]);
+
+  return <p ref={ref}>LOADING...</p>;
 }
+
+/*
+fetch data for page 1
+set loading to false
+
+*/
 
 const DataList = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const {list, loading} = useDataList({pageNumber});
 
+  const updatePageNumber = () => {
+    setPageNumber(prev => prev + 1);
+  }
+
   return (<div>
   <ul>
     {list.map((username) => <li>{username}</li>)}
   </ul>
-  <Footer />
+  <Footer loading={loading} onScreenCallback={updatePageNumber}/>
   </div>)
 }
 
